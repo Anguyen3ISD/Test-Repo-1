388d4db2eb9fa3f62030bcfa494cd8161084ea5f
Anh Tuan Nguyen <anguyen5@dhs.lacounty.gov>
2024-09-16 07:19:17 -0700

    Change 1 test

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-        Console.WriteLine("This is a .NET 8 Console/CLI test project.");
+        Console.WriteLine("This is a .NET 8 Console/CL test project.");
+        Console.WriteLine("Test test test.");
     }
 }
